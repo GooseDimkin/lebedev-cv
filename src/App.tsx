267390af6eb48d71dcfe59bbde4cb932b32106e0
DmLebedev <lebedevdmitry7660882@gmail.com>
2024-11-14 18:02:39 +0200

    feat: untapped project

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import profilePicture from "src/assets/profilePicture.jpg";
+
 import leadQuiz1 from "src/assets/projects/leadQuiz/leadQuiz1.jpg";
 import leadQuiz2 from "src/assets/projects/leadQuiz/leadQuiz2.jpg";
 import leadQuiz3 from "src/assets/projects/leadQuiz/leadQuiz3.jpg";
@@ -16,6 +17,17 @@ import leadQuiz7 from "src/assets/projects/leadQuiz/leadQuiz7.jpg";
 import leadQuiz8 from "src/assets/projects/leadQuiz/leadQuiz8.jpg";
 import leadQuiz9 from "src/assets/projects/leadQuiz/leadQuiz9.jpg";
 import leadQuiz10 from "src/assets/projects/leadQuiz/leadQuiz10.jpg";
+
+import untapped1 from "src/assets/projects/untapped/untapped1.jpg";
+import untapped2 from "src/assets/projects/untapped/untapped2.jpg";
+import untapped3 from "src/assets/projects/untapped/untapped3.jpg";
+import untapped4 from "src/assets/projects/untapped/untapped4.jpg";
+import untapped5 from "src/assets/projects/untapped/untapped5.jpg";
+import untapped6 from "src/assets/projects/untapped/untapped6.jpg";
+import untapped7 from "src/assets/projects/untapped/untapped7.jpg";
+import untapped8 from "src/assets/projects/untapped/untapped8.jpg";
+import untapped9 from "src/assets/projects/untapped/untapped9.jpg";
+import untapped10 from "src/assets/projects/untapped/untapped10.jpg";
 
 const projects = [
   {
@@ -34,12 +46,25 @@ const projects = [
     title: "LeadQuiz",
     url: "https://leadquiz.com",
     description:
-      "As a front-end developer, I developed a website that allows users to generate survey forms. Buy subscriptions. And the owner of the site has administrator rights and access to the admin page to edit and add new templates for forms, create or edit new plans for subscriptions. The stack of technologies that I used: JavaScript, TypeScript, Redux, REST API, Stripe.",
+      "I developed a website that allows users to generate survey forms. Buy subscriptions. And the owner of the site has administrator rights and access to the admin page to edit and add new templates for forms, create or edit new plans for subscriptions. The stack of technologies that I used: JavaScript, TypeScript, Redux, REST API, Stripe.",
   },
   {
-    images: [leadQuiz1, leadQuiz1],
-    title: "",
-    description: "",
+    images: [
+      untapped1,
+      untapped2,
+      untapped3,
+      untapped4,
+      untapped5,
+      untapped6,
+      untapped7,
+      untapped8,
+      untapped9,
+      untapped10,
+    ],
+    title: "Untapped",
+    url: "https://untapped-global.com",
+    description:
+      "I created a react components for project. As always, I worked with the team, and we are used GitHub for version controls of project. At that stage of developing, there wasn't any backend connections, so main tasks was making front side styling. For styling the components, client wanted us to use Styled Components technology. All technologies that was included to the project: React.js, TypeScript, Styled components.",
   },
   {
     images: [leadQuiz1, leadQuiz1],
@@ -122,7 +147,7 @@ function App() {
           <motion.div
             key={index}
             className={styles.projectCard}
-            initial={{ opacity: 0, y: 50 }}
+            initial={{ opacity: 0, y: 200 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: index * 0.2 }}
             viewport={{ once: true }}
@@ -146,7 +171,7 @@ function App() {
       </section>
 
       <motion.div
-        initial={{ opacity: 0, y: 50 }}
+        initial={{ opacity: 0, y: 200 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.2 }}
         viewport={{ once: true }}
